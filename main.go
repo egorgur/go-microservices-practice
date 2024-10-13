769835af7b900aabe325de634d5b38247fd5b7c9
Egor Gurtyakin <egorgurtyakin@gmail.com>
2024-10-13 17:14:54 +0700

    example server with std http package

--- main.go
+++ main.go
@@ -1,20 +1,47 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/egorgur/go-microsevices-practice/handlers"
 )
 
 func main() {
-	l := log.New(os.Stdout, "go-server", log.LstdFlags)
+	l := log.New(os.Stdout, "go-server:", log.LstdFlags)
 	hh := handlers.NewHello(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
 
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
 
-	http.ListenAndServe(":9090", sm)
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	s.Shutdown(tc)
 }
