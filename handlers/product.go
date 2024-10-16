1c258de03caa7947b278b78279f9d2a614898404
Egor Gurtyakin <egorgurtyakin@gmail.com>
2024-10-16 21:27:58 +0700

    use gorilla mux and use middleware

--- handlers/product.go
+++ handlers/product.go
@@ -1,11 +1,13 @@
 package handlers
 
 import (
+	"context"
 	"log"
 	"net/http"
-	"regexp"
 	"strconv"
+
 	"github.com/egorgur/go-microservices-practice/product-api/data"
+	"github.com/gorilla/mux"
 )
 
 type Products struct {
@@ -16,54 +18,53 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		p.l.Println("Handle GET request")
-		p.getProducts(rw, r)
-		return
-	}
+// func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+// 	if r.Method == http.MethodGet {
+// 		p.l.Println("Handle GET request")
+// 		p.getProducts(rw, r)
+// 		return
+// 	}
 
-	if r.Method == http.MethodPost {
-		p.l.Println("Handle POST request")
-		p.addProduct(rw, r)
-		return
-	}
+// 	if r.Method == http.MethodPost {
+// 		p.l.Println("Handle POST request")
+// 		p.addProduct(rw, r)
+// 		return
+// 	}
 
-	if r.Method == http.MethodPut {
-		// expect the id in the URI
-		p.l.Println("Handle PUT request")
-		re := regexp.MustCompile(`/([0-9]+)`)
-		foundMatches := re.FindAllStringSubmatch(r.URL.Path, -1)
-		
-		if len(foundMatches) != 1 {
-			http.Error(rw, "Invalid URL", http.StatusBadRequest)
-			p.l.Printf("Invalid URL parsing %#v", foundMatches)
-			return
-		}
+// 	if r.Method == http.MethodPut {
+// 		// expect the id in the URI
+// 		p.l.Println("Handle PUT request")
+// 		re := regexp.MustCompile(`/([0-9]+)`)
+// 		foundMatches := re.FindAllStringSubmatch(r.URL.Path, -1)
 
-		if len(foundMatches[0]) != 2 {
-			http.Error(rw, "Invalid URL", http.StatusBadRequest)
-			p.l.Printf("Invalid URL parsing %#v", foundMatches)
-			return
-		}
+// 		if len(foundMatches) != 1 {
+// 			http.Error(rw, "Invalid URL", http.StatusBadRequest)
+// 			p.l.Printf("Invalid URL parsing %#v", foundMatches)
+// 			return
+// 		}
 
-		idString := foundMatches[0][1]
-		id, err := strconv.Atoi(idString)
-		if err != nil {
-			http.Error(rw, "Failed to parse index from URL", http.StatusBadRequest)
-			p.l.Fatalf("Invalid URL parsing %#v", idString)
-			return
-		}
+// 		if len(foundMatches[0]) != 2 {
+// 			http.Error(rw, "Invalid URL", http.StatusBadRequest)
+// 			p.l.Printf("Invalid URL parsing %#v", foundMatches)
+// 			return
+// 		}
 
-		p.updateProduct(rw, r, id)
-	}
+// 		idString := foundMatches[0][1]
+// 		id, err := strconv.Atoi(idString)
+// 		if err != nil {
+// 			http.Error(rw, "Failed to parse index from URL", http.StatusBadRequest)
+// 			p.l.Fatalf("Invalid URL parsing %#v", idString)
+// 			return
+// 		}
 
+// 		p.updateProduct(rw, r, id)
+// 	}
 
-	// catch all not implemented
-	rw.WriteHeader(http.StatusMethodNotAllowed)
-}
+// 	// catch all not implemented
+// 	rw.WriteHeader(http.StatusMethodNotAllowed)
+// }
 
-func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
 	lp := data.GetProducts()
 	err := lp.ToJSON(rw)
 	if err != nil {
@@ -72,26 +73,24 @@ func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
-	prod := &data.Product{}
-	err := prod.FromJSON(r.Body)
-	if err != nil {
-		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
-		return
-	}
+func (p *Products) AddProduct(rw http.ResponseWriter, r *http.Request) {
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
 	p.l.Printf("Prod %#v", prod)
-	data.AddProduct(prod)
+	data.AddProduct(&prod)
 }
 
-func (p *Products) updateProduct(rw http.ResponseWriter, r *http.Request, id int) {
-	prod := &data.Product{}
-	err := prod.FromJSON(r.Body)
+func (p *Products) UpdateProduct(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	idString := vars["id"]
+	id, err := strconv.Atoi(idString)
 	if err != nil {
-		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+		http.Error(rw, "Failed to parse index from URL", http.StatusBadRequest)
+		p.l.Fatalf("Invalid URL parsing %#v", idString)
 		return
 	}
-	p.l.Printf("Prod %#v", prod)
-	err = data.UpdateProduct(prod, id)
+
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
+	err = data.UpdateProduct(&prod, id)
 	if err == data.ErrProductNotFound {
 		http.Error(rw, "Unable to found the resource", http.StatusNotFound)
 		return
@@ -100,4 +99,22 @@ func (p *Products) updateProduct(rw http.ResponseWriter, r *http.Request, id int
 		http.Error(rw, "Unable to update the resource", http.StatusInternalServerError)
 		return
 	}
+}
+
+type KeyProduct struct{}
+
+func (p Products) MiddlewareProductValidation(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		prod := data.Product{}
+
+		err := prod.FromJSON(r.Body)
+		if err != nil {
+			http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), KeyProduct{}, prod)
+		req := r.WithContext(ctx)
+		next.ServeHTTP(rw, req)
+	})
 }
--- main.go
+++ main.go
@@ -8,15 +8,30 @@ import (
 	"os/signal"
 	"time"
 
+	"github.com/gorilla/mux"
+
 	"github.com/egorgur/go-microservices-practice/handlers"
 )
 
 func main() {
 	l := log.New(os.Stdout, "go-server:", log.LstdFlags)
+
+	//Handlers
 	ph := handlers.NewProducts(l)
 
-	sm := http.NewServeMux()
-	sm.Handle("/", ph)
+	// Gorilla serve mux
+	sm := mux.NewRouter()
+
+	getRouter := sm.Methods(http.MethodGet).Subrouter()
+	getRouter.HandleFunc("/", ph.GetProducts)
+
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProduct)
+	putRouter.Use(ph.MiddlewareProductValidation)
+
+	postRouter := sm.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", ph.AddProduct)
+	postRouter.Use(ph.MiddlewareProductValidation)
 
 	s := &http.Server{
 		Addr:         ":9090",
